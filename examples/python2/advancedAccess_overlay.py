b072d9991d62c908904b32ddb22c377e4121e7e3
Michal Tykac <Michal.Tykac@gmail.com>
2020-05-02 23:46:24 +0200

    Added the support for python2 to be installed under command python2 instead of python. Also, changed the examples to assume global installation and therefore commented out the adition of the local module path.

--- examples/python2/advancedAccess_overlay.py
+++ examples/python2/advancedAccess_overlay.py
@@ -26,7 +26,7 @@ import sys
 import numpy
 
 ### Import ProSHADE from non-system folder (local installation assumed)
-sys.path.append                               ( "/Users/mysak/BioCEV/proshade/experimental/install/python2" )
+#sys.path.append                               ( "/Users/mysak/BioCEV/proshade/experimental/install/python2" )
 import proshade
 
 ### Create the object
--- examples/python3/simpleAccess_overlay.py
+++ examples/python3/simpleAccess_overlay.py
@@ -27,7 +27,7 @@ import sys
 import numpy
 
 ### Import ProSHADE from non-system folder (local installation assumed)
-sys.path.append                               ( "/Users/mysak/BioCEV/proshade/experimental/install/python3" )
+#sys.path.append                               ( "/Users/mysak/BioCEV/proshade/experimental/install/python3" )
 import proshade
 
 ##############################################
