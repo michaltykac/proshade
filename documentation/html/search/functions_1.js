b17c4249bdb1d8747086521bba4bb0ed0de32fe6
Michal Tykac <Michal.Tykac@gmail.com>
2020-09-06 15:03:51 +0200

    Deleted documentation in preparation for another merge

--- documentation/html/search/functions_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['beautifyboundaries_667',['beautifyBoundaries',['../namespace_pro_s_h_a_d_e__internal__map_manip.html#ae273430e12dd5bce08cf29c7689800dd',1,'ProSHADE_internal_mapManip']]],
-  ['bettercloseprimefactors_668',['betterClosePrimeFactors',['../namespace_pro_s_h_a_d_e__internal__map_manip.html#abc6c505ea6b876bfae4d5f45dab63e98',1,'ProSHADE_internal_mapManip']]],
-  ['blursharpenmap_669',['blurSharpenMap',['../namespace_pro_s_h_a_d_e__internal__map_manip.html#afdac8ed8f140bdcd289ee666b61789d4',1,'ProSHADE_internal_mapManip']]]
-];
--- documentation/html/search/variables_5.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['filename_1074',['fileName',['../class_pro_s_h_a_d_e__internal__data_1_1_pro_s_h_a_d_e__data.html#ae9b0d5959b0f461b3b89ffeb059400fa',1,'ProSHADE_internal_data::ProSHADE_data']]],
-  ['filetype_1075',['fileType',['../class_pro_s_h_a_d_e__internal__data_1_1_pro_s_h_a_d_e__data.html#a4052e498b4650f62bfe5ff37ed959292',1,'ProSHADE_internal_data::ProSHADE_data']]],
-  ['forcebounds_1076',['forceBounds',['../class_pro_s_h_a_d_e__settings.html#aa3e24ef5a66fa7204a2ba9c27f2f12b9',1,'ProSHADE_settings']]]
-];
