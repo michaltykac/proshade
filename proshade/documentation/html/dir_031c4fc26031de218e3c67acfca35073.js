b2124d93dd9437d9d4f3584238fa6517aeb603ca
Michal Tykac <Michal.Tykac@gmail.com>
2021-02-01 12:56:52 +0100

    Removed documentation to facilitate merge with development

--- proshade/documentation/html/dir_031c4fc26031de218e3c67acfca35073.js
+++
@@ -1,4 +0,0 @@
-var dir_031c4fc26031de218e3c67acfca35073 =
-[
-    [ "bin.cpp", "bin_8cpp.html", "bin_8cpp" ]
-];
--- proshade/documentation/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['proshade_20documentation_1344',['ProSHADE Documentation',['../index.html',1,'']]]
-];
