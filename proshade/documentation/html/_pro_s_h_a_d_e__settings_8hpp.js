49467926ffb139ee1c4af39ea9fef43b74c1d848
Michal Tykac <Michal.Tykac@gmail.com>
2021-07-05 21:25:21 +0200

    Removed documentation in preparation for new version merge.

--- proshade/documentation/html/_pro_s_h_a_d_e__settings_8hpp.js
+++
@@ -1,5 +0,0 @@
-var _pro_s_h_a_d_e__settings_8hpp =
-[
-    [ "ProSHADE_settings", "class_pro_s_h_a_d_e__settings.html", "class_pro_s_h_a_d_e__settings" ],
-    [ "__PROSHADE_SETTINGS__", "_pro_s_h_a_d_e__settings_8hpp.html#adbd9e29a67267d0dbd58997b09ebbab6", null ]
-];
--- proshade/documentation/html/search/variables_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['halfmapkernel_1241',['halfMapKernel',['../class_pro_s_h_a_d_e__settings.html#a8d2299cb1a7b0470e0f26e431af2548f',1,'ProSHADE_settings']]]
-];
